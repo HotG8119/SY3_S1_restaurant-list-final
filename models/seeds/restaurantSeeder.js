e9a011420e19db471aff6ab4c5073ff5c89fc3d7
HotG8119 <k508060@gmail.com>
2023-07-14 21:42:01 +0800

    feat: update seeder.js

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -40,16 +40,27 @@ const SEED_USER = [
   {
     email: "user1@example.com",
     password: "12345678",
-    restaurantId: [1, 2, 3],
+    restaurantId: [0, 1, 2],
   },
   {
-    email: "user1@example.com",
+    email: "user2@example.com",
     password: "12345678",
-    restaurantId: [4, 5, 6],
+    restaurantId: [3, 4, 5],
   },
 ];
 
 db.once("open", () => {
+  const createRestaurant = (userId, restaurantIds) => {
+    return Promise.all(
+      restaurantIds.map(id =>
+        Restaurant.create({
+          ...SEED_RESTAURANTS[id],
+          userId,
+        })
+      )
+    );
+  };
+
   bcrypt
     .genSalt(10)
     .then(salt => bcrypt.hash(SEED_USER[0].password, salt))
@@ -60,8 +71,20 @@ db.once("open", () => {
         password: hash,
       })
     )
+    .then(user => createRestaurant(user._id, [0, 1, 2]))
+
+    .then(() => bcrypt.genSalt(10))
+    .then(salt => bcrypt.hash(SEED_USER[1].password, salt))
+    .then(hash =>
+      User.create({
+        name: SEED_USER[1].name,
+        email: SEED_USER[1].email,
+        password: hash,
+      })
+    )
+    .then(user => createRestaurant(user._id, [3, 4, 5]))
+
     .then(() => {
-      console.log("done.");
-      process.exit();
+      console.log("create seed done"), process.exit();
     });
 });
